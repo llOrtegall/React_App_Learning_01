88a7cd074d45387c77d1feff724bbf944b4eb384
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-03-04 15:05:03 -0500

    agregando stilos simples

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import './App.css'
+
 function App() {
 
   return (
-    <>
-      <h1>Helo Aqui</h1>
-    </>
+    <div className='page'>
+
+      <header>
+        <h1>Bucador De Peliculas</h1>
+        <form className="form">
+          <input placeholder="Avengers, Matrix, Nemo..." />
+          <button type="submit">Buscar</button>
+        </form>
+      </header>
+
+      <main>
+        Aquí irán los resultados
+      </main>
+
+    </div>
   )
 }
 
