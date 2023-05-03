e38aaf7cec9ddfea81ce2b64d6d7de0663f35a3b
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-05-03 16:18:30 -0500

    punto de entrada creado

--- main.jsx
+++ main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
 
-const root = createRoot(document.getElementById('app'));
+const root = createRoot(document.getElementById('root'));
 
-root.render(<h1>Hello World</h1>)
+root.render(<h1>Hello World</h1>);
