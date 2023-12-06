ea5acad517cf0fde433e4373f3e1c3b58cfb42f3
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-12-06 14:23:46 -0500

    pausa para acomodar la API

--- src/pages/VerMovimiento.jsx
+++ src/pages/VerMovimiento.jsx
@@ -1,5 +1,16 @@
-export function VerMovimiento(){
-  return(
+import axios from "axios"
+import { useEffect } from "react"
+
+export function VerMovimiento() {
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/getMovimientos")
+      .then(response => {
+        console.log(response.data)
+      })
+  }, [])
+
+  return (
     <h1>Ver Movimiento</h1>
   )
 }
