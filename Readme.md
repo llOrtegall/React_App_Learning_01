a79002e86830e98bddca57304a546261fb24d1e5
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-07-09 02:11:35 -0500

    continuar con prueba tecnica junior

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Prueba Técnica - Desarrollador Junior
+
+## Objetivo
+APIS 
+
+- Facts Ramdom: https://carfact.ninja/fact
+- Imagen Ramdom: https://cataas.com/cat/says/hello
+
+  - endpoint para usar: `https://cataas.com/cat/says/${palabras}?fontSize=50&fontColor=red&json=true`
+
+## Requerimientos
+
+- Recupera un hecho aleatorio de la API de hechos y una imagen de la API 
+- Recuperar la primera palabra del hecho
+- Mostra una imágenes. con la primera palbra del hecho recuperado usando la segunda API
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,44 @@
+import { useEffect, useState } from 'react'
+
+const CAT_ENDPOINT = 'https://catfact.ninja/fact'
+
 function App () {
+  const [fact, setFact] = useState('lorem impsun car fact whatever')
+  const [img, setImg] = useState()
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+      })
+  }, [])
+
+  useEffect(() => {
+    const firstWords = fact.split(' ', 3).join(' ')
+
+    if (!fact) return
+
+    fetch(`https://cataas.com/cat/says/${firstWords}?fontSize=50&fontColor=red`)
+      .then(response => {
+        const { url } = response
+        setImg(url)
+      })
+  }, [fact])
+
   return (
-    <div className='bg-blue-500'>
-      <h1>My First React App</h1>
-    </div>
+    <section className='flex flex-col h-screen w-full bg-gray-800 text-white p-2 '>
+      <h1 className='font-semibold py-6 text-2xl text-center'>App De Gatitos</h1>
+      <div className='flex flex-col items-center justify-center'>
+        {fact && <article className='m-4 px-2'>{fact}</article>}
+        {img && <img
+          className='' src={img} alt='ìmagen extraida desde una cat fact con 3 palabras'
+          width={400} loading='lazy'
+                />}
+      </div>
+    </section>
+
   )
 }
 
